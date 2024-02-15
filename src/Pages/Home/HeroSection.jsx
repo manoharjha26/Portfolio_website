45e01a312e31c93af912569d17e474d20626667e
manoharjha26 <aryabdwaj26@gmail.com>
2024-02-15 16:13:10 -0500

    first commit

---
+++ src/Pages/Home/HeroSection.jsx
@@ -0,0 +1,26 @@
+export default function HeroSection() {
+  return (
+    <section id="heroSection" className="hero--section">
+      <div className="hero--section--content--box">
+        <div className="hero--section--content">
+          <p className="section--title">Hey, I'm Manohar</p>
+          <h1 className="hero--section--title">
+            <span className="hero--section-title--color">Full Stack</span>{" "}
+            <br />
+            Developer
+          </h1>
+          <p className="hero--section-description">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            <br /> Dolorum, quas. Amet soluta assumenda cum?
+          </p>
+        </div>
+        <button className="btn btn-primaryi">
+          <a href="https://www.canva.com/design/DAFskNMCOS4/7VAJsxTMYtSFp-Sufm21hg/view?utm_content=DAFskNMCOS4&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink" className="github-btn btn-h">Resume</a>
+        </button>
+      </div>
+      <div className="hero--section--img">
+        <img src="./img/hero_img.png" alt="Hero Section" />
+      </div>
+    </section>
+  );
+}
