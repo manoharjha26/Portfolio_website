f89a8b54d97a7636fb86c7bd542fa9005b8574d5
manoharjha26 <aryabdwaj26@gmail.com>
2024-02-15 17:19:46 -0500

    a

--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -10,12 +10,17 @@ export default function HeroSection() {
             Developer
           </h1>
           <p className="hero--section-description">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-            <br /> Dolorum, quas. Amet soluta assumenda cum?
+            I am a result-oriented and self-motivated MCA student looking <br />{" "}
+            for a challenging career where I can implement my coding knowledge
           </p>
         </div>
         <button className="btn btn-primary">
-          <a href="https://www.canva.com/design/DAFskNMCOS4/7VAJsxTMYtSFp-Sufm21hg/view?utm_content=DAFskNMCOS4&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink" className="github-btn btn-h">Resume</a>
+          <a
+            href="https://www.canva.com/design/DAFskNMCOS4/7VAJsxTMYtSFp-Sufm21hg/view?utm_content=DAFskNMCOS4&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink"
+            className="github-btn btn-h"
+          >
+            Resume
+          </a>
         </button>
       </div>
       <div className="hero--section--img">
